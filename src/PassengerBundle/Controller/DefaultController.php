4352cde29324187ae34bc77a156d739def1d09a9
Anthony Plummer <aplummer@psda.com>
2016-11-18 23:32:57 -0500

    Restructured Base Layout for Blockchain build

--- src/PassengerBundle/Controller/DefaultController.php
+++ src/PassengerBundle/Controller/DefaultController.php
@@ -19,9 +19,33 @@ class DefaultController extends Controller
 
     /**
      * @Route("/{page}", name="static")
-     * @Template("@PassengerBundle::default/index.html")
+     * @Template()
      */
     public function indexAction($page) {
+        $ip = $this->getIpAddress();
+        return array('ip' => $ip);
         return array('page' => $page);
+    }
+
+    public function getIpAddress() {
+      // Function to get the client IP address
+      $ipaddress = '';
+      if (getenv('HTTP_CLIENT_IP'))
+          $ipaddress = getenv('HTTP_CLIENT_IP');
+      else if(getenv('HTTP_X_FORWARDED_FOR'))
+          $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+      else if(getenv('HTTP_X_FORWARDED'))
+          $ipaddress = getenv('HTTP_X_FORWARDED');
+      else if(getenv('HTTP_FORWARDED_FOR'))
+          $ipaddress = getenv('HTTP_FORWARDED_FOR');
+      else if(getenv('HTTP_FORWARDED'))
+         $ipaddress = getenv('HTTP_FORWARDED');
+      else if(getenv('REMOTE_ADDR'))
+          $ipaddress = getenv('REMOTE_ADDR');
+      else
+          $ipaddress = 'UNKNOWN';
+
+      return $ipaddress;
+      return array('ip' => $ipaddress);
     }
 }
